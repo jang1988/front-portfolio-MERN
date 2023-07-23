617649ad9b05e79d37aef8667b309dd5100ff7e0
Baklay Petro <pustota1988@gmail.com>
2023-07-23 11:45:02 +0300

    creeteRoutes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import BurgerMenu from './components/BurgerMenu';
 import Header from './components/Header';
-import Main from './components/Main';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Services from './pages/Services';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-
     const [menuActive, setMenuActive] = React.useState(false);
-    
+
     const items = [
         { value: 'Главная', href: '/', icon: 'chevron_left' },
         { value: 'О нас', href: '/about', icon: 'anchor' },
@@ -17,9 +21,21 @@ function App() {
     return (
         <div className="App">
             <Header active={menuActive} setActive={setMenuActive} />
-            <Main />
-            <Main />
-            <BurgerMenu active={menuActive} setActive={setMenuActive} header={'Меню'} items={items} />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+
+            <BurgerMenu
+                active={menuActive}
+                setActive={setMenuActive}
+                header={'Меню'}
+                items={items}
+            />
         </div>
     );
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from '../../components/Main';
+
+const Home = () => {
+    return (
+        <>
+            <Main />
+            <Main />
+        </>
+    );
+};
+
+export default Home;
